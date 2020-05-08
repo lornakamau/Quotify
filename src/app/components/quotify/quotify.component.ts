6ca209561fcedcad4eb1eaf4f25e78562f68e95b
Lorna Kamau <kamaulorna@gmail.com>
2020-05-08 12:12:58 +0300

    add card to display quotes

--- src/app/components/quotify/quotify.component.ts
+++ src/app/components/quotify/quotify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote} from '../../models/quote';
 
 @Component({
   selector: 'app-quotify',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotifyComponent implements OnInit {
 
+  quotes:Quote[] = [
+    new Quote('If you can make a woman laugh, you can make her do anything.','Marilyn Monroe','Lyn Adelaide',new Date(2020,4,8)),
+    new Quote('There is nothing I would not do for those who are really my friends. I have no notion of loving people by halves, it is not my nature.','Jane Austen','Edrea Jakes',new Date(2020,3,30)),
+    new Quote('A friend is someone who knows all about you and still loves you.','Elbert Hubbard','Lizbeth Wangu',new Date(2020,1,5)),
+    new Quote('It is not a lack of love, but a lack of friendship that makes unhappy marriages.','Friedrich Nietzsche','Charlene Rose',new Date(2019,9,11)),
+    new Quote('The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.',' Bob Marley','Albert Austen',new Date(2019,5,15)),
+  ];  
+  toggleDetails(index){
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
   constructor() { }
 
   ngOnInit(): void {
