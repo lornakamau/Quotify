659a39eba0c25805326c22dd7a5581f79d0be380
Lorna Kamau <kamaulorna@gmail.com>
2020-05-08 11:35:33 +0300

    add assets and update favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotifyComponent } from './components/quotify/quotify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotifyComponent
   ],
   imports: [
     BrowserModule,
