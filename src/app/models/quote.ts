738565133289bcc196a8c1dfb79efb0e1ced3983
Lorna Kamau <kamaulorna@gmail.com>
2020-05-09 10:28:57 +0300

    add function that toggles the small text

--- src/app/models/quote.ts
+++ src/app/models/quote.ts
@@ -1,6 +1,8 @@
 export class Quote {
     showDetails: boolean;
-    constructor(public quote: string,public author: string,public submitter: string,public submissionDate: Date,public upVote: number,public downVote: number){
+    showMore: boolean;
+    constructor(public quote: string,public author: string,public submitter: string,public submissionDate: Date,public upVote: number,public downVote: number,public smallText:any = 'see more'){
     this.showDetails=false;
+    this.showMore=false;
   }
 }
